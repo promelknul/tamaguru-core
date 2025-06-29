86b7bbe96b6895db8b3e4d7a85394fba0fe07a5c
promelknul <you@example.com>
2025-06-30 00:35:56 +0300

    feat(vault): HTTP /auth/login handler returning JWT

--- core/vault/cmd/main.go
+++ core/vault/cmd/main.go
@@ -1,3 +1,14 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+
+	vaulthttp "github.com/promelknul/tamaguru-core/core/vault/http"
+)
+
+func main() {
+	http.HandleFunc("/auth/login", vaulthttp.LoginHandler)
+	log.Println("vaultd HTTP :9300 up")
+	log.Fatal(http.ListenAndServe(":9300", nil))
+}
